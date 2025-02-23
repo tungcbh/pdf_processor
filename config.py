66fd4d69ac97aa90baf9a76cc0e4f010b8cbf4c6
Tùng Nguyễn Thanh <nttung42@outlook.com>
2025-02-23 07:36:27 +0700

    fix: edit database config

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+# Tải biến môi trường từ file .env (nếu có)
+load_dotenv()
+
 class DBConfig:
-    HOST = "localhost"
-    DATABASE = "pdf_processor"
-    USER = "postgres"
-    PASSWORD = "123456"
-    PORT = 5433
+    HOST = os.getenv("DB_HOST", "localhost")
+    DATABASE = os.getenv("DB_NAME", "pdf_processor")
+    USER = os.getenv("DB_USER", "postgres")
+    PASSWORD = os.getenv("DB_PASSWORD", "123456")
+    PORT = int(os.getenv("DB_PORT", "5432"))
